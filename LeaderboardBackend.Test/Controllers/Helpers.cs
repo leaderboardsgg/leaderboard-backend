8e66305ce06da0d43b7eba44f3cf1b71bc26328d
Braydon Kains <kainsbraydon@gmail.com>
2022-01-13 20:13:49 -0500

    UsersController Unit Tests (#11)
    * Add UsersController Unit Tests
    
    * Nicer error message from GetValue helper
    
    GetValueFromObjectResult previously allowed the exception to fail the
    test, but it's more usable to catch the exception and Assert.Fail with
    the message.
    
    Also added .editorconfig to Solution Items so formatting is consistent.
    
    * Fix formatting
    
    Visual Studio totally butchered the tabbing on these files and I'm not
    sure why. I formatted them all to the editorconfig, and will make a
    followup PR to unify the rest of the formatting.
    
    * Change GetValueFromObjectResult line setup
    
    Syntax highlighting didn't work in VSCode or GitHub for some reason with
    this setup.
    
    * Change test fixture class name

---
+++ LeaderboardBackend.Test/Controllers/Helpers.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+
+namespace LeaderboardBackend.Test.Controllers
+{
+	internal static class Helpers
+	{
+		public static void AssertResponseNotFound<T>(ActionResult<T> response)
+		{
+			var actual = response.Result as NotFoundResult;
+			Assert.NotNull(actual);
+			Assert.AreEqual(404, actual!.StatusCode);
+		}
+
+		public static void AssertResponseBadRequest<T>(ActionResult<T> response)
+		{
+			var actual = response.Result as BadRequestResult;
+			Assert.NotNull(actual);
+			Assert.AreEqual(400, actual!.StatusCode);
+		}
+
+		public static void AssertResponseForbid<T>(ActionResult<T> response)
+		{
+			var actual = response.Result as ForbidResult;
+			Assert.NotNull(actual);
+			// ForbidResult inherits from ActionResult, not StatusCodeResult. Unlike
+			// the previous assertions, this is sufficient to know we have received
+			// a Forbidden 403 result.
+		}
+
+		public static TResult? GetValueFromObjectResult<TObjectResult, TResult>(
+			ActionResult<TResult> result
+		) where TObjectResult : ObjectResult
+		{
+			TObjectResult? objectResult = null;
+			try
+			{
+				objectResult = (TObjectResult?)result?.Result;
+			}
+			catch (InvalidCastException e)
+			{
+				Assert.Fail(e.Message);
+			}
+			return (TResult?)objectResult?.Value;
+		}
+	}
+}
