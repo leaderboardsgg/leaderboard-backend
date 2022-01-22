f5bbb35e0a03e7de3148f488af9487b633b9c7cb
RageCage64 <kainsbraydon@gmail.com>
2022-01-21 22:16:33 -0500

    Add Json Serialization Option

--- LeaderboardBackend/Program.cs
+++ LeaderboardBackend/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using LeaderboardBackend.Services;
 using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
 
 JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
@@ -19,7 +20,10 @@ builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase(DbName));
 builder.Services.AddDbContext<LeaderboardContext>(opt => opt.UseInMemoryDatabase(DbName));
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(opt =>
+{
+	opt.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
