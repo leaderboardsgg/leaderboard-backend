b4aac2af5bd2e6a0c00b5d9a4060bdd4c0ee4376
Sim <me@zhongyuen.dev>
2022-01-23 10:56:24 +0800

    Postgres Support Yayyyy

--- LeaderboardBackend/Program.cs
+++ LeaderboardBackend/Program.cs
@@ -5,14 +5,43 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using LeaderboardBackend.Services;
 using System.IdentityModel.Tokens.Jwt;
-using System.Text.Json;
+using dotenv.net;
+using dotenv.net.Utilities;
 
 JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
-// FIXME: temp
-const string DbName = "Leaderboard";
-
 var builder = WebApplication.CreateBuilder(args);
+
+DotEnv.Load(options: new DotEnvOptions(
+	ignoreExceptions: false, // Notifies of exceptions during loading of .env
+	envFilePaths: new[] { "../.env" },
+	trimValues: true // Trims whitespace from values
+));
+
+static string GetConnectionString()
+{
+	string host, user, password, db;
+	int port;
+
+	if (
+		!EnvReader.TryGetStringValue("POSTGRES_HOST", out host) ||
+		!EnvReader.TryGetIntValue("POSTGRES_PORT", out port) ||
+		!EnvReader.TryGetStringValue("POSTGRES_USER", out user) ||
+		!EnvReader.TryGetStringValue("POSTGRES_PASSWORD", out password) ||
+		!EnvReader.TryGetStringValue("POSTGRES_DB", out db)
+	)
+	{
+		throw new Exception("Database env var(s) not set. Is there a .env?");
+	}
+	return String.Format(
+		"Server={0};Port={1};User Id={2};Password={3};Database={4};Include Error Detail=true",
+		host,
+		port,
+		user,
+		password,
+		db
+	);
+}
 
 // Add services to the container.
 builder.Services.AddScoped<IUserService, UserService>();
@@ -20,10 +49,7 @@ builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase(DbName));
 builder.Services.AddDbContext<LeaderboardContext>(opt => opt.UseInMemoryDatabase(DbName));
-builder.Services.AddControllers().AddJsonOptions(opt =>
-{
-	opt.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-});
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
