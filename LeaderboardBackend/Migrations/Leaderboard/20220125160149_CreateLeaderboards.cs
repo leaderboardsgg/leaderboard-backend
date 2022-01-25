2d1efe2c29342f81f202fd96af4e1ae0e2c59e04
Sim <me@zhongyuen.dev>
2022-01-26 00:03:11 +0800

    Create Leaderboard Migrations

---
+++ LeaderboardBackend/Migrations/Leaderboard/20220125160149_CreateLeaderboards.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LeaderboardBackend.Migrations.Leaderboard
+{
+    public partial class CreateLeaderboards : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "leaderboards",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    slug = table.Column<string>(type: "text", nullable: false),
+                    rules = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_leaderboards", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "leaderboards");
+        }
+    }
+}
