987fdd2a35f0e121ba937bdef0be8aa9293e161d
Sim <me@zhongyuen.dev>
2022-01-29 01:06:35 +0800

    Add Custom Error Messages

--- LeaderboardBackend/Controllers/Requests/Register.cs
+++ LeaderboardBackend/Controllers/Requests/Register.cs
@@ -4,7 +4,8 @@ namespace LeaderboardBackend.Controllers.Requests;
 public record RegisterRequest
 {
 	[Required]
-	[RegularExpression("/([a-zA-Z][-_']?){1,12}[a-zA-Z]/")]
+	[RegularExpression("/([a-zA-Z][-_']?){1,12}[a-zA-Z]/",
+		ErrorMessage = "Your name must be between 2 and 25 characters, made up of letters sandwiching zero or one hyphen, underscore, or apostrophe.")]
 	public string Username { get; set; } = null!;
 
 	[Required]
@@ -12,14 +13,13 @@ public record RegisterRequest
 	public string Email { get; set; } = null!;
 
 	[Required]
-	[MinLength(8)]
-	[MaxLength(80)]
+	[MinLength(8, ErrorMessage = "Your password must be at least 8 characters long.")]
+	[MaxLength(80, ErrorMessage = "Your password must be at most 80 characters long.")]
 	[Password]
 	public string Password { get; set; } = null!;
 
 	[Required]
-	[MinLength(8)]
-	[MaxLength(80)]
+	[Compare("Password", ErrorMessage = "This must match your password.")]
 	[Password]
 	public string PasswordConfirm { get; set; } = null!;
 }
--- LeaderboardBackend/Models/User.cs
+++ LeaderboardBackend/Models/User.cs
@@ -6,12 +6,16 @@ public class User
 {
 	[Required] public Guid Id { get; set; }
 
+	[RegularExpression(@"^([a-zA-Z][-_']?){1,12}[a-zA-Z]$",
+		ErrorMessage = "Your name must be between 2 and 25 characters, made up of letters sandwiching zero or one hyphen, underscore, or apostrophe.")]
+	public string? Username { get; set; }
+
 	[Required]
 	[EmailAddress]
 	public string? Email { get; set; }
 
 	[Required]
-	[MinLength(8)]
-	[MaxLength(80)]
+	[MinLength(8, ErrorMessage = "Your password must be at least 8 characters long.")]
+	[MaxLength(80, ErrorMessage = "Your password must be at most 80 characters long.")]
 	public string Password { get; set; } = null!;
 }
