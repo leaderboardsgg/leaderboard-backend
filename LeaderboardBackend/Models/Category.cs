2c1a167ae3ac810c8484c673d9478add86fc738f
Sim <me@zhongyuen.dev>
2022-02-02 18:55:35 +0800

    Add Model and Relationships

---
+++ LeaderboardBackend/Models/Category.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace LeaderboardBackend.Models
+{
+	public class Category
+	{
+		public ulong Id { get; set; }
+		[Required] public string Name { get; set; } = null!;
+		[Required] public string Slug { get; set; } = null!;
+		public string? Rules { get; set; }
+		[Required] public int PlayersMin { get; set; }
+		[Required] public int PlayersMax { get; set; }
+		[Required] public ulong LeaderboardId { get; set; }
+		[Required] [JsonIgnore] public Leaderboard? Leaderboard { get; set; }
+	}
+}
