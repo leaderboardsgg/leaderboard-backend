9b97230a8560d0be8a9110301a8a0ab563013f6d
zysim <9867871+zysim@users.noreply.github.com>
2022-02-06 10:06:10 +0000

    [REVIEW FIRST] Postgres Support (#23)
    * Add Postgres Support
    
    * Add docker-compose.yml
    And edit .editorconfig rules cuz docker-compose.yml doesn't like tabs
    for indentation lmao
    
    * Add example.env
    
    * Postgres Support Yayyyy
    
    * Remove Copy of docker-compose.yml
    
    * Define Backend Port in appsettings.json Instead
    
    * Add DB Docs
    
    * Added in memory database option, VS config stuff
    
    Added a few files to the solution items for Visual Studio users. Added a
    way to retain in memory database functionality if someone wants to
    develop just on the in memory database for whatever reason.
    
    * readded camel case config that I clobbered
    
    * readded solution items that I clobbered
    
    * Add in memory DB instructions to README
    
    * Fix Type Cast
    
    Co-authored-by: Sim <me@zhongyuen.dev>
    Co-authored-by: RageCage64 <kainsbraydon@gmail.com>

--- LeaderboardBackend/Program.cs
+++ LeaderboardBackend/Program.cs
@@ -5,31 +5,76 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using LeaderboardBackend.Services;
 using System.IdentityModel.Tokens.Jwt;
+using dotenv.net;
+using dotenv.net.Utilities;
 using System.Text.Json;
 
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-
-// FIXME: temp
-const string DbName = "Leaderboard";
-
 var builder = WebApplication.CreateBuilder(args);
+
+DotEnv.Load(options: new DotEnvOptions(
+	ignoreExceptions: false, // Notifies of exceptions during loading of .env
+	envFilePaths: new[] { "../.env" },
+	trimValues: true // Trims whitespace from values
+));
+
+// Add Database Contexts to the container.
+static string GetConnectionString()
+{
+	if (
+		!EnvReader.TryGetStringValue("POSTGRES_HOST", out string host) ||
+		!EnvReader.TryGetIntValue("POSTGRES_PORT", out int port) ||
+		!EnvReader.TryGetStringValue("POSTGRES_USER", out string user) ||
+		!EnvReader.TryGetStringValue("POSTGRES_PASSWORD", out string password) ||
+		!EnvReader.TryGetStringValue("POSTGRES_DB", out string db)
+	)
+	{
+		throw new Exception("Database env var(s) not set. Is there a .env?");
+	}
+	return $"Server={host};Port={port};User Id={user};Password={password};Database={db};Include Error Detail=true";
+}
+
+static void ConfigureDbContext<T>(WebApplicationBuilder builder, bool inMemoryDb) where T : DbContext {
+	if (inMemoryDb)
+	{
+		builder.Services.AddDbContext<T>(opt => opt.UseInMemoryDatabase("LeaderboardBackend"));
+	} else
+	{
+		builder.Services.AddDbContext<T>(
+			opt => {
+				opt.UseNpgsql(
+					GetConnectionString()
+				).UseSnakeCaseNamingConvention();
+			}
+		);
+	}
+}
+
+bool exists = EnvReader.TryGetBooleanValue("USE_IN_MEMORY_DB", out bool inMemoryDb);
+bool useInMemoryDb = exists && inMemoryDb;
+
+ConfigureDbContext<UserContext>(builder, useInMemoryDb);
+ConfigureDbContext<LeaderboardContext>(builder, useInMemoryDb);
 
 // Add services to the container.
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase(DbName));
-builder.Services.AddDbContext<LeaderboardContext>(opt => opt.UseInMemoryDatabase(DbName));
+
+// Add controllers to the container.
 builder.Services.AddControllers().AddJsonOptions(opt =>
 {
 	opt.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
 });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
 	c.SwaggerDoc("v1", new() { Title = "LeaderboardBackend", Version = "v1" });
 });
+
+// Configure JWT Authentication.
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 	.AddJwtBearer(options =>
 	{
