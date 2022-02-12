86fb86b45029afead2a42fc84bc79081aff43c51
Sim <me@zhongyuen.dev>
2022-02-12 11:08:54 +0800

    Add Docs for User Model

--- LeaderboardBackend/Controllers/Requests/Register.cs
+++ LeaderboardBackend/Controllers/Requests/Register.cs
@@ -1,21 +1,41 @@
 using System.ComponentModel.DataAnnotations;
 namespace LeaderboardBackend.Controllers.Requests;
 
-public record RegisterRequest 
+public record RegisterRequest
 {
+	/// <summary>User's name.</summary>
+	/// <example>Ayylmao Gaming</example>
 	[Required]
 	[RegularExpression("(?:[a-zA-Z][-_']?){1,12}[a-zA-Z]",
 		ErrorMessage = "Your name must be between 2 and 25 characters, made up of letters sandwiching zero or one hyphen, underscore, or apostrophe.")]
 	public string Username { get; set; } = null!;
 
+	/// <summary>User's email.</summary>
+	/// <example>ayylmao.gaming@alg.gg</example>
 	[Required]
 	[EmailAddress]
 	public string Email { get; set; } = null!;
 
+	/// <summary>
+	/// User's password. It:
+	/// <ul>
+	///   <li>must be 8-80 characters long, inclusive;</li>
+	///   <li>must have at least:</li>
+	///     <ul>
+	///       <li>an uppercase letter;</li>
+	///       <li>a lowercase letter; and</li>
+	///       <li>a number.</li>
+	///     </ul>
+	///   <li>supports Unicode.</li>
+	/// </ul>
+	/// </summary>
+	/// <example>P4ssword</example>
 	[Required]
 	[Password]
 	public string Password { get; set; } = null!;
 
+	/// <summary>Confirmation of the User's password. This <em>must</em> match Password.</summary>
+	/// <example>P4ssword</example>
 	[Required]
 	[Compare("Password", ErrorMessage = "The password confirmation must match your password.")]
 	public string PasswordConfirm { get; set; } = null!;
--- LeaderboardBackend/Models/User.cs
+++ LeaderboardBackend/Models/User.cs
@@ -1,12 +1,31 @@
 using System.ComponentModel.DataAnnotations;
-using LeaderboardBackend.Controllers.Requests;
+using System.Text.Json.Serialization;
 
-namespace LeaderboardBackend.Models;
-
-public class User
+namespace LeaderboardBackend.Models
 {
-	public Guid Id { get; set; }
-	[Required] public string Username { get; set; } = null!;
-	[Required] [EmailAddress] public string Email { get; set; } = null!;
-	[Required] [Password] public string Password { get; set; } = null!;
+	public class User
+	{
+		/// <summary>A GUID that identifies the User. Generated on creation.</summary>
+		/// <example>4b3835ca-dee1-4019-82b4-d2d26a7cce74</example>
+		public Guid Id { get; set; }
+		/// <summary>
+		/// The User's name. Must be:
+		/// <ul>
+		///   <li>between 2 - 25 characters inclusive; and</li>
+		///   <li>a sequence of letters, each separated by zero or one of:</li>
+		///   <ul>
+		///     <li>an underscore;</li>
+		///     <li>a hyphen; or</li>
+		///     <li>an apostrophe</li>
+		///   </ul>
+		/// </ul>
+		/// Saving a name is case-sensitive, but matching against existing Users won't be.
+		/// </summary>
+		/// <example>Ayylmao Gaming</example>
+		[Required] public string Username { get; set; } = null!;
+		/// <summary>The User's email. Must be, well, an email.</summary>
+		/// <example>ayylmao.gaming@alg.gg</example>
+		[Required] public string Email { get; set; } = null!;
+		[Required] [JsonIgnore] public string Password { get; set; } = null!;
+	}
 }
