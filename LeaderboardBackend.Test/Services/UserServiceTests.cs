6b9c0fd051b0775303f7ecfbc82dd8a54521e524
Sim <me@zhongyuen.dev>
2022-02-13 10:10:58 +0800

    Revert "Complete UserService Tests"
    This reverts commit 6fa9d6c0eaa3a7e51945567e32af832af53c9929.

--- LeaderboardBackend.Test/Services/UserServiceTests.cs
+++ LeaderboardBackend.Test/Services/UserServiceTests.cs
@@ -3,18 +3,18 @@ using LeaderboardBackend.Services;
 using LeaderboardBackend.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
-using System;
+using Moq;
 using System.Threading.Tasks;
 
 namespace LeaderboardBackend.Test.Services;
 
 public class UserServiceTests
 {
-	private static Guid _userId = Guid.Parse("44818346-23b7-4f96-87e0-6edcabd4244c");
-	private static UserService _service = null!;
-	private static User _testUser = new User
-	{
-		Id = _userId,
+	private static IConfiguration _mockConfig = null!;
+	private static Mock<UserContext> _mockContext = null!;
+	private static Mock<DbSet<User>> _mockDbSet = null!;
+	private static User _user = new User {
+		Id = 1,
 		Username = "RageCage",
 		Email = "x@y.com"
 	};
@@ -38,60 +38,56 @@ public class UserServiceTests
 		);
 	}
 
-	private static UserContext GetUserContext()
-	{
-		return new UserContext(
-			new DbContextOptionsBuilder<UserContext>().UseNpgsql(
-				"Host=localhost;Port=5433;Database=leaderboardstest;Username=admin;Password=example;"
-			).Options
-		);
-	}
-
 	[SetUp]
 	public void Setup()
 	{
-		_service = new UserService(GetUserContext(), GetMockConfig());
+		_mockConfig = GetMockConfig();
+		_mockDbSet = new Mock<DbSet<User>>();
+		var options = new DbContextOptions<UserContext>();
+		_mockContext = new Mock<UserContext>(options);
+		_mockContext.Setup(m => m.Users).Returns(_mockDbSet.Object);
 	}
 
 	[Test]
 	public async Task CreateUser()
 	{
-		// TODO: the DB call gives "System.IO.EndOfStreamException: Attempted to read past the end of the stream"
-		await _service.CreateUser(_testUser);
-		Assert.NotNull(await _service.GetUser(_userId));
+		var service = new UserService(_mockContext.Object, _mockConfig);
 
-		// TODO: Call this too
-		// _mockDbSet.Verify(m => m.Add(It.IsAny<User>()), Times.Once());
-		// _mockContext.Verify(m => m.SaveChangesAsync(default), Times.Once());
+		await service.CreateUser(_user);
+
+		_mockDbSet.Verify(m => m.Add(It.IsAny<User>()), Times.Once());
+		_mockContext.Verify(m => m.SaveChangesAsync(default), Times.Once());
 	}
 
 	[Test]
 	public async Task GetUser_GetsAnExistingUser()
 	{
-		var result = await _service.GetUser(_userId);
+		_mockDbSet.Setup(m => m.FindAsync(It.IsAny<long>()).Result).Returns(_user);
+
+		var service = new UserService(_mockContext.Object, _mockConfig);
+		var result = await service.GetUser(1);
 
 		Assert.NotNull(result);
-		Assert.AreEqual(_userId, result?.Id);
+		Assert.AreEqual(1, result?.Id);
 		Assert.AreEqual("RageCage", result?.Username);
+		_mockDbSet.Verify(m => m.FindAsync(It.IsAny<long>()), Times.Once());
 	}
 
 	[Test]
 	public async Task GetUser_ReturnsNullForNonExistingID()
 	{
-		var result = await _service.GetUser(Guid.NewGuid());
+		_mockDbSet.Setup(m => m.FindAsync(1).Result).Returns(_user);
+
+		var service = new UserService(_mockContext.Object, _mockConfig);
+		var result = await service.GetUser(2);
 
 		Assert.Null(result);
+		_mockDbSet.Verify(m => m.FindAsync((long)2), Times.Once());
 	}
 
-	// TODO: Implement this + fail states
-	// [Test]
-	// public async Task GetUserByEmail_GetsUser()
-	// {
-	// }
-
-	// TODO: Implement this + fail states
-	// [Test]
-	// public async Task GetUserFromClaims_GetsUser()
-	// {
-	// }
+	// GetUserByEmail and GetUserFromClaims can't be unit tested with the current
+	// strategy, as Moq is unable to mock extension methods (SingleAsync,
+	// FindFirstValue).
+	// Tests will need to be run against an actual database, by which we should remove
+	// mocking in this file.
 }
