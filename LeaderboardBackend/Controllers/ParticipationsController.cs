b51680b14eb408079e496d2f68d79819d86f0fdb
Sim <me@zhongyuen.dev>
2022-02-15 02:20:42 +0800

    Add CreateParticipation Action

--- LeaderboardBackend/Controllers/ParticipationsController.cs
+++ LeaderboardBackend/Controllers/ParticipationsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using LeaderboardBackend.Models;
 using LeaderboardBackend.Services;
+using LeaderboardBackend.Controllers.Requests;
 
 namespace LeaderboardBackend.Controllers
 {
@@ -9,10 +10,18 @@ namespace LeaderboardBackend.Controllers
 	public class ParticipationsController : ControllerBase
 	{
 		private readonly IParticipationService _participationService;
+		private readonly IRunService _runService;
+		private readonly IUserService _userService;
 
-		public ParticipationsController(IParticipationService participationService)
+		public ParticipationsController(
+			IParticipationService participationService,
+			IRunService runService,
+			IUserService userService
+		)
 		{
 			_participationService = participationService;
+			_runService = runService;
+			_userService = userService;
 		}
 
 		[HttpGet("{id}")]
@@ -25,6 +34,46 @@ namespace LeaderboardBackend.Controllers
 			}
 
 			return Ok(participation);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> CreateParticipation([FromBody] CreateParticipationRequest request)
+		{
+			// TODO: Maybe create validation middleware
+			if (request.IsRunner && (request.Comment == null || request.Vod == null))
+			{
+				return BadRequest("You must provide a comment and a VoD link, as you're the submitter.");
+			}
+
+			var runner = await _userService.GetUser(request.RunnerId);
+			var run = await _runService.GetRun(request.RunId);
+
+			if (runner == null)
+			{
+				if (request.IsRunner)
+				{
+					return BadRequest("We can't add your participation because we can't.. find.. your ID? On our end???");
+				}
+				return BadRequest("We can't find the user with the provided ID on our end.");
+			}
+
+			if (run == null)
+			{
+				return BadRequest("We can't find the associated run on our end.");
+			}
+
+			Participation participation = new Participation
+			{
+				Comment = request.Comment,
+				RunId = request.RunId,
+				Run = run,
+				RunnerId = request.RunnerId,
+				Runner = runner,
+				Vod = request.Vod
+			};
+
+			await _participationService.CreateParticipation(participation);
+			return CreatedAtAction(nameof(CreateParticipation), new { id = participation.Id });
 		}
 	}
 }
