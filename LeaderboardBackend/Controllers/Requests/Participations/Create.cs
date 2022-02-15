5e712d98450bdad5d580dbc7ec141d29e0dae369
Sim <me@zhongyuen.dev>
2022-02-16 00:39:34 +0800

    Add Rudimentary Validation on Requests

--- LeaderboardBackend/Controllers/Requests/Participations/Create.cs
+++ LeaderboardBackend/Controllers/Requests/Participations/Create.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LeaderboardBackend.Controllers.Requests
 {
 	public class CreateParticipationRequest
@@ -6,10 +8,13 @@ namespace LeaderboardBackend.Controllers.Requests
 
 		public string? Vod { get; set; }
 
+		[Required]
 		public Guid RunnerId { get; set; }
 
+		[Required]
 		public long RunId { get; set; }
 
+		[Required]
 		public bool IsSubmitter { get; set; } = true;
 	}
 }
--- LeaderboardBackend/Controllers/Requests/Participations/Update.cs
+++ LeaderboardBackend/Controllers/Requests/Participations/Update.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LeaderboardBackend.Controllers.Requests
 {
 	public class UpdateParticipationRequest
 	{
-		public string Comment { get; set; } = null!;
+		public string Comment { get; set; } = "";
 
+		[Required(ErrorMessage = "Please add a VoD link. Your participation can't be confirmed otherwise.")]
 		public string Vod { get; set; } = null!;
 	}
 }
