7b40196c52dbed44001e078d7334d76bfb1e9c10
Sim <me@zhongyuen.dev>
2022-02-25 23:30:52 +0800

    Re-add Null Forgiveness in User Model

--- LeaderboardBackend/Models/User.cs
+++ LeaderboardBackend/Models/User.cs
@@ -7,13 +7,14 @@ public class User
 {
 	[Required] public Guid Id { get; set; }
 
+	[Required]
 	[RegularExpression(@"^([a-zA-Z][-_']?){1,12}[a-zA-Z]$",
 		ErrorMessage = "Your name must be between 2 and 25 characters, made up of letters sandwiching zero or one hyphen, underscore, or apostrophe.")]
-	public string? Username { get; set; }
+	public string Username { get; set; } = null!;
 
 	[Required]
 	[EmailAddress]
-	public string? Email { get; set; }
+	public string Email { get; set; } = null!;
 
 	[Required]
 	[Password]
