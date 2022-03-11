7503d48ff8269d4d9a82c2ff1e0a0d9fdb55d3f8
Sim <me@zhongyuen.dev>
2022-03-12 02:16:45 +0800

    Remove Unnecessary Validation Attribute

--- LeaderboardBackend/Models/User.cs
+++ LeaderboardBackend/Models/User.cs
@@ -8,8 +8,6 @@ public class User
 	[Required] public Guid Id { get; set; }
 
 	[Required]
-	[RegularExpression(@"^([a-zA-Z][-_']?){1,12}[a-zA-Z]$",
-		ErrorMessage = "Your name must be between 2 and 25 characters, made up of letters sandwiching zero or one hyphen, underscore, or apostrophe.")]
 	public string Username { get; set; } = null!;
 
 	[Required]
