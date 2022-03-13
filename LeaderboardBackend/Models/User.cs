da6805d7ecef4eb067d9b08edb60eab454c820e8
Sim <me@zhongyuen.dev>
2022-03-13 18:34:06 +0800

    Update User Attributes

--- LeaderboardBackend/Models/User.cs
+++ LeaderboardBackend/Models/User.cs
@@ -5,16 +5,8 @@ namespace LeaderboardBackend.Models;
 
 public class User
 {
-	[Required] public Guid Id { get; set; }
-
-	[Required]
-	public string Username { get; set; } = null!;
-
-	[Required]
-	[EmailAddress]
-	public string Email { get; set; } = null!;
-
-	[Required]
-	[Password]
-	public string Password { get; set; } = null!;
+	public Guid Id { get; set; }
+	[Required] public string Username { get; set; } = null!;
+	[Required] [EmailAddress] public string Email { get; set; } = null!;
+	[Required] [Password] public string Password { get; set; } = null!;
 }
