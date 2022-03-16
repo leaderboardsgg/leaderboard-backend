7f784e2381ab399f039dcdb4a6eb18d73c1dae85
Sim <me@zhongyuen.dev>
2022-03-16 22:00:14 +0800

    Remove Method Prefix Matches The prefixes are for when you're applying the entire convention class as an attribute to a controller. Since we apply the convention methods individually to controller actions, it's actually unused.

--- LeaderboardBackend/Controllers/Conventions.cs
+++ LeaderboardBackend/Controllers/Conventions.cs
@@ -7,7 +7,6 @@ public static class Conventions
 {
 	[ProducesResponseType(StatusCodes.Status200OK)]
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
-	[ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
 	public static void Get(
 		[ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]
 		[ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
@@ -17,7 +16,6 @@ public static class Conventions
 
 	[ProducesResponseType(StatusCodes.Status201Created)]
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
-	[ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
-	public static void Create(params object[] parameters)
+	public static void Post(params object[] parameters)
 	{ }
 }
