c1b89ec4cfc993bb1a46be71a678c7269b34700d
RageCage64 <kainsbraydon@gmail.com>
2022-03-20 13:27:02 -0400

    Leaderboard integration tests (#69)
    * Restructured Models, added Models from all PRs
    
    * Add Users integration tests
    
    * Add Integration Test setup
    
    * Add IntegrationTest project
    * Include .env in build for integration test project
    * Use `Program.cs` as entry point
    * Use `Staging` environment for integration tests
    * db_test now mounts the correct port on the container
    * Adjust env var for test port (test DB will now share name)
    
    * Add Users integration tests
    
    * Add Leaderboard Integration Tests
    
    * Making the tests work with rebases

--- LeaderboardBackend.Test/Helpers/ApplicationContextFactory.cs
+++ LeaderboardBackend.Test/Helpers/ApplicationContextFactory.cs
@@ -1,10 +1,5 @@
-using LeaderboardBackend.Models;
+using LeaderboardBackend.Models.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeaderboardBackend.Test.Helpers;
 
