953489ef20f0c44298a89c49441957efe3d88243
Sim <me@zhongyuen.dev>
2022-03-23 01:14:37 +0800

    Use Helper Context Factory for CategoryService Unit Tests

--- LeaderboardBackend.Test/Services/CategoryServiceTests.cs
+++ LeaderboardBackend.Test/Services/CategoryServiceTests.cs
@@ -1,7 +1,6 @@
 using NUnit.Framework;
 using LeaderboardBackend.Services;
-using LeaderboardBackend.Models;
-using Microsoft.EntityFrameworkCore;
+using LeaderboardBackend.Test.Helpers;
 using System.Threading.Tasks;
 
 namespace LeaderboardBackend.Test.Services;
@@ -13,22 +12,26 @@ public class CategoryServiceTests
 	[SetUp]
 	public void Setup()
 	{
-		_service = new CategoryService(
-			new ApplicationContext(
-				new DbContextOptionsBuilder<ApplicationContext>().UseNpgsql(
-					"Host=localhost;Port=5433;Database=leaderboardstest;Username=admin;Password=example;"
-				).Options
-			)
-		);
+		_service = new CategoryService(ApplicationContextFactory.CreateNewContext());
 	}
 
 	[Test]
 	public async Task GetCategory_GetsAnExistingCategory()
 	{
+		await _service.CreateCategory(new() {
+			LeaderboardId = 1,
+			Name = "Test",
+			Slug = "test",
+			PlayersMax = 1,
+			PlayersMin = 1
+		});
+
 		var result = await _service.GetCategory(1);
 
 		Assert.NotNull(result);
 		Assert.AreEqual(1, result?.Id);
+		Assert.AreEqual(1, result?.PlayersMax);
+		Assert.AreEqual(1, result?.PlayersMin);
 	}
 
 	[Test]
