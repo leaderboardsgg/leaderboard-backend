374c633ca531355bf51737a563bc50d428d4881d
Sim <me@zhongyuen.dev>
2022-03-24 22:41:36 +0800

    Migrations

--- LeaderboardBackend/Migrations/20220324144053_Users_CompleteFields.Designer.cs
+++ LeaderboardBackend/Migrations/20220324144053_Users_CompleteFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LeaderboardBackend.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LeaderboardBackend.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220324144053_Users_CompleteFields")]
+    partial class Users_CompleteFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
