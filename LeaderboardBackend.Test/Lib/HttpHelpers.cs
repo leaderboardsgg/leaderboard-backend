81f02ec22b6ddebb7ded35d9e36d514e4c82e58b
Sim <me@zhongyuen.dev>
2022-04-15 00:07:54 +0800

    Create Request Helpers with Auth

--- LeaderboardBackend.Test/Lib/HttpHelpers.cs
+++ LeaderboardBackend.Test/Lib/HttpHelpers.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Json;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -12,11 +13,35 @@ internal static class HttpHelpers
 {
 	/// <typeparam name="Body">Object class that defines the POST's request body.</typeparam>
 	/// <typeparam name="Res">Response object class from the POST.</typeparam>
-	public static async Task<Res> Post<Body, Res>(string endpoint, Body requestBody, HttpClient httpClient, JsonSerializerOptions options)
+	public static async Task<Res> Get<Res>(
+		string endpoint,
+		HttpClient client,
+		JsonSerializerOptions options,
+		string? jwt = null
+	)
 	{
-		HttpResponseMessage response = await httpClient.PostAsJsonAsync(endpoint, requestBody, options);
-		response.EnsureSuccessStatusCode();
-		return await HttpHelpers.ReadFromResponseBody<Res>(response, options);
+		return await Send<Res>(
+			CreateRequestMessage(endpoint, HttpMethod.Get, options, jwt),
+			client,
+			options
+		);
+	}
+
+	/// <typeparam name="Body">Object class that defines the POST's request body.</typeparam>
+	/// <typeparam name="Res">Response object class from the POST.</typeparam>
+	public static async Task<Res> Post<Body, Res>(
+		string endpoint,
+		Body body,
+		HttpClient client,
+		JsonSerializerOptions options,
+		string? jwt = null
+	)
+	{
+		return await Send<Res>(
+			CreateRequestMessage(endpoint, HttpMethod.Post, options, jwt, body),
+			client,
+			options
+		);
 	}
 
 	public static async Task<T> ReadFromResponseBody<T>(HttpResponseMessage response, JsonSerializerOptions jsonOptions)
@@ -31,5 +56,37 @@ internal static class HttpHelpers
 	{
 		IEnumerable<string> queryList = list.Select(l => $"{key}={l}");
 		return string.Join("&", queryList);
+	}
+
+	private static HttpRequestMessage CreateRequestMessage(
+		string endpoint,
+		HttpMethod method,
+		JsonSerializerOptions options,
+		string? jwt,
+		object? body = null
+	)
+	{
+		HttpRequestMessage requestMessage = new(method, endpoint);
+
+		if (body is not null)
+		{
+			StringContent content = new(JsonSerializer.Serialize(body, options));
+			content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+			requestMessage.Content = content;
+		}
+
+		if (jwt is not null)
+		{
+			requestMessage.Headers.Authorization = new AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, jwt);
+		}
+
+		return requestMessage; // To be then called with HttpClient.SendAsync(requestMessage);
+	}
+
+	private static async Task<Res> Send<Res>(HttpRequestMessage message, HttpClient client, JsonSerializerOptions options)
+	{
+		HttpResponseMessage response = await client.SendAsync(message);
+		response.EnsureSuccessStatusCode();
+		return await HttpHelpers.ReadFromResponseBody<Res>(response, options);
 	}
 }
