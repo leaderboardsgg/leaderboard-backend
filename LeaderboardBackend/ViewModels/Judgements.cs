91508085c56fa293cdeb672161a5d05b6990c771
Sim <me@zhongyuen.dev>
2022-05-21 23:47:11 +0800

    If This Actually Needs to be Done

--- LeaderboardBackend/ViewModels/Judgements.cs
+++ LeaderboardBackend/ViewModels/Judgements.cs
@@ -3,41 +3,41 @@ using LeaderboardBackend.Models.Entities;
 namespace LeaderboardBackend.ViewModels;
 
 /// <summary>A decision by a mod on a run submission. See Models/Entities/Judgement.cs.</summary>
-public record JudgementViewModel
+public record JudgementViewModel(long Id, bool? Approved, string CreatedAt, string? Note, Guid ModId, Guid RunId)
 {
 	/// <summary>The newly-made judgement's ID.</summary>
-	public long Id { get; set; }
+	internal long Id { get; set; } = Id;
 
 	/// <summary>
 	///   The judgement result. Can be true, false, or null. In the latter two, <code>Note</code> will be non-empty.
 	/// </summary>
-	public bool? Approved { get; set; }
+	internal bool? Approved { get; set; } = Approved;
 
 	/// <summary>
 	///   When the judgement was made. Follows <a href="https://www.ietf.org/rfc/rfc3339.txt">RFC 3339</a>.
 	/// </summary>
 	/// <example>2022-01-01T12:34:56Z / 2022-01-01T12:34:56+01:00</example>
-	public string CreatedAt { get; set; }
+	internal string CreatedAt { get; set; } = CreatedAt;
 
 	/// <summary>
 	///   Judgement comments. Acts as mod feedback for the runner. Will be non-empty for
 	///   non-approval judgements (Approved is false or null).
 	/// </summary>
-	public string? Note { get; set; }
+	internal string? Note { get; set; } = Note;
 
 	/// <summary>ID of mod who made this judgement.</summary>
-	public Guid ModId { get; set; }
+	internal Guid ModId { get; set; } = ModId;
 
 	/// <summary>ID of run this judgement's for.</summary>
-	public Guid RunId { get; set; }
+	internal Guid RunId { get; set; } = RunId;
 
-	public JudgementViewModel(Judgement judgement)
-	{
-		Id = judgement.Id;
-		Approved = judgement.Approved;
-		CreatedAt = judgement.CreatedAt.ToLongDateString();
-		Note = judgement.Note;
-		ModId = judgement.ModId;
-		RunId = judgement.RunId;
-	}
+	public JudgementViewModel(Judgement judgement) : this
+	(
+		judgement.Id,
+		judgement.Approved,
+		judgement.CreatedAt.ToLongDateString(),
+		judgement.Note,
+		judgement.ModId,
+		judgement.RunId
+	) {}
 }
