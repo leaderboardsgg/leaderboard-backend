123300ed7fa150cab0e0589ce8c540f85d82b3aa
RageCage64 <kainsbraydon@gmail.com>
2022-06-04 11:36:08 -0400

    Remove unwanted access modifiers from interface

--- LeaderboardBackend/Services/IAuthService.cs
+++ LeaderboardBackend/Services/IAuthService.cs
@@ -6,6 +6,6 @@ namespace LeaderboardBackend.Services;
 public interface IAuthService
 {
 	string GenerateJSONWebToken(User user);
-	public string? GetEmailFromClaims(ClaimsPrincipal claims);
-	public Guid? GetUserIdFromClaims(ClaimsPrincipal claims);
+	string? GetEmailFromClaims(ClaimsPrincipal claims);
+	Guid? GetUserIdFromClaims(ClaimsPrincipal claims);
 }
