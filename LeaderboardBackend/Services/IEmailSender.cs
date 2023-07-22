d04ca1ba1aae8b2e9e1f0ac5e6cf95fa281b1125
Dalet <dmolet.gh@outlook.com>
2023-07-22 11:28:30 +0200

    add summary for IEmailSender.EnqueueEmailAsync

--- LeaderboardBackend/Services/IEmailSender.cs
+++ LeaderboardBackend/Services/IEmailSender.cs
@@ -2,5 +2,12 @@ namespace LeaderboardBackend.Services;
 
 public interface IEmailSender
 {
+    /// <summary>
+    /// Enqueues an email for sending.
+    /// </summary>
+    /// <param name="recipientAddress">The email address of the recipient</param>
+    /// <param name="subject">Email subject</param>
+    /// <param name="htmlMessage">Email message in HTML format</param>
+    /// <remarks>Throws an exception on queuing failure.</remarks>
     Task EnqueueEmailAsync(string recipientAddress, string subject, string htmlMessage);
 }
