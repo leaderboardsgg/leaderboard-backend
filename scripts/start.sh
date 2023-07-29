3a8485123d5c0d164f631ebfebec91226a35a30c
Edward Runkel <eddierunkel@gmail.com>
2023-07-28 21:35:07 -0400

    Remove `--urls` param

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 dotnet tool restore
 docker compose up -d
 dotnet ef database update --project LeaderboardBackend
-dotnet run --project LeaderboardBackend --urls https://localhost:7128
+dotnet run --project LeaderboardBackend
