ed10edce6081044e811f93ac726491d6b81da7db
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2023-08-15 14:32:01 -0400

    Remove in-memory DB options from configs.

--- LeaderboardBackend.Test/Fixtures/TestConfig.cs
+++ LeaderboardBackend.Test/Fixtures/TestConfig.cs
@@ -4,23 +4,8 @@ namespace LeaderboardBackend.Test.Fixtures;
 
 internal static class TestConfig
 {
-    public static DatabaseBackend DatabaseBackend { get; private set; } =
-        DatabaseBackend.TestContainer;
-
     static TestConfig()
     {
-        string? backendVar = Environment.GetEnvironmentVariable("INTEGRATION_TESTS_DB_BACKEND");
-        if (Enum.TryParse(backendVar, out DatabaseBackend dbBackend))
-        {
-            DatabaseBackend = dbBackend;
-        }
-
         Bogus.Randomizer.Seed = new Random(43817269); // fixed seed for repeatable tests
     }
-}
-
-internal enum DatabaseBackend
-{
-    InMemory,
-    TestContainer,
 }
