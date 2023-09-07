c98db21da1849466abd2f1e5b11bd089f32a32c5
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2023-09-07 15:15:31 -0400

    Create Features class.

---
+++ LeaderboardBackend/Models/Features.cs
@@ -0,0 +1,7 @@
+namespace LeaderboardBackend.Models;
+
+public static class Features
+{
+    public const string ACCOUNT_REGISTRATION = "AccountRegistration";
+    public const string LOGIN = "Login";
+}
