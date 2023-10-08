ae55267a8d8a4e7fc7dd8b58f081baea6141f29a
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2023-10-07 21:55:54 -0400

    Add new result.

--- LeaderboardBackend/Results.cs
+++ LeaderboardBackend/Results.cs
@@ -7,5 +7,6 @@ public readonly record struct BadRole();
 public readonly record struct ConfirmationNotFound();
 public readonly record struct EmailFailed();
 public readonly record struct Expired();
+public readonly record struct Old();
 public readonly record struct UserNotFound();
 public readonly record struct UserBanned();
