d7c0f72042c6f77240072f2d6b6fe92d21ff9f91
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2023-11-07 17:28:55 -0500

    Convert UserController XML comments to annotations.

--- LeaderboardBackend/Controllers/UsersController.cs
+++ LeaderboardBackend/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using LeaderboardBackend.Models.ViewModels;
 using LeaderboardBackend.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using BCryptNet = BCrypt.Net.BCrypt;
 
 namespace LeaderboardBackend.Controllers;
@@ -20,16 +21,15 @@ public class UsersController : ApiController
         _userService = userService;
     }
 
-    /// <summary>
-    ///     Gets a User by their ID.
-    /// </summary>
-    /// <param name="id">The ID of the `User` which should be retrieved.</param>
-    /// <response code="200">The `User` was found and returned successfully.</response>
-    /// <response code="404">No `User` with the requested ID could be found.</response>
     [AllowAnonymous]
     [ApiConventionMethod(typeof(Conventions), nameof(Conventions.GetAnon))]
     [HttpGet("{id:guid}")]
-    public async Task<ActionResult<UserViewModel>> GetUserById(Guid id)
+    [SwaggerOperation("Gets a User by their ID.")]
+    [SwaggerResponse(200, "The `User` was found and returned successfully.")]
+    [SwaggerResponse(404, "No `User` with the requested ID could be found.")]
+    public async Task<ActionResult<UserViewModel>> GetUserById(
+        [SwaggerParameter("The ID of the `User` which should be retrieved.")] Guid id
+    )
     {
         User? user = await _userService.GetUserById(id);
 
@@ -41,22 +41,19 @@ public class UsersController : ApiController
         return Ok(UserViewModel.MapFrom(user));
     }
 
-    /// <summary>
-    ///     Gets the currently logged-in User.
-    /// </summary>
-    /// <remarks>
-    ///     Call this method with the 'Authorization' header. A valid JWT bearer token must be
-    ///     passed.<br/>
-    ///     Example: `{ 'Authorization': 'Bearer JWT' }`.
-    /// </remarks>
-    /// <response code="200">The `User` was found and returned successfully..</response>
-    /// <response code="401">An invalid JWT was passed in.</response>
-    /// <response code="404">The user was not found in the database.</response>
-    [HttpGet("me")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ApiConventionMethod(typeof(Conventions), nameof(Conventions.Get))]
+    [HttpGet("me")]
+    [SwaggerOperation(
+        "Gets the currently logged-in User.",
+        """
+        Call this method with the 'Authorization' header. A valid JWT bearer token must be
+        passed.<br/>
+        Example: `{ 'Authorization': 'Bearer JWT' }`.
+        """
+    )]
+    [SwaggerResponse(200, "The `User` was found and returned successfully.")]
+    [SwaggerResponse(401, "An invalid JWT was passed in.")]
+    [SwaggerResponse(404, "The user was not found in the database.")]
     public async Task<ActionResult<UserViewModel>> Me()
     {
         return (await _userService.GetUserFromClaims(HttpContext.User)).Match<ActionResult<UserViewModel>>(
