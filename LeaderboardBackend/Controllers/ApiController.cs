203370d1b40491a759eaa87d5b75727da481a45c
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-06-30 21:51:45 -0400

    Add 400 and 500 errors to every controller.

--- LeaderboardBackend/Controllers/ApiController.cs
+++ LeaderboardBackend/Controllers/ApiController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace LeaderboardBackend.Controllers;
 
@@ -6,4 +7,6 @@ namespace LeaderboardBackend.Controllers;
 [Consumes("application/json")]
 [Produces("application/json")]
 [Route("api/[controller]")]
+[SwaggerResponse(400, Type = typeof(ProblemDetails))]
+[SwaggerResponse(500)]
 public abstract class ApiController : ControllerBase { }
