8ae3a5fdbd9f9bef7ae9b50c4309d7bdd45ec172
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-07-18 17:40:17 -0400

    Remove unnecessary LB request defaults.

--- LeaderboardBackend/Models/Requests/LeaderboardRequests.cs
+++ LeaderboardBackend/Models/Requests/LeaderboardRequests.cs
@@ -9,12 +9,12 @@ public record CreateLeaderboardRequest
     ///     The display name of the `Leaderboard` to create.
     /// </summary>
     /// <example>Foo Bar</example>
-    public required string Name { get; set; } = null!;
+    public required string Name { get; set; }
 
     /// <summary>
     ///     The URL-scoped unique identifier of the `Leaderboard`.<br/>
     ///     Must be [2, 80] in length and consist only of alphanumeric characters and hyphens.
     /// </summary>
     /// <example>foo-bar</example>
-    public required string Slug { get; set; } = null!;
+    public required string Slug { get; set; }
 }
