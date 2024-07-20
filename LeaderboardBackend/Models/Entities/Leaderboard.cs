5c531defe6a0f23088bf123e49491c583f38c2c8
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-07-20 19:58:25 -0400

    Add uniqueness constraint to Leaderboard Slug.

--- LeaderboardBackend/Models/Entities/Leaderboard.cs
+++ LeaderboardBackend/Models/Entities/Leaderboard.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 using NodaTime;
 
 namespace LeaderboardBackend.Models.Entities;
@@ -6,6 +7,7 @@ namespace LeaderboardBackend.Models.Entities;
 /// <summary>
 ///     Represents a collection of `Category` entities.
 /// </summary>
+[Index(nameof(Slug), IsUnique = true)]
 public class Leaderboard
 {
     /// <summary>
