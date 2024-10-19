425367b24ef7796e13472bd0279b98b73e6620ea
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-10-19 16:05:59 -0400

    Add DeleteResult.

--- LeaderboardBackend/Results.cs
+++ LeaderboardBackend/Results.cs
@@ -1,9 +1,12 @@
+using OneOf;
+using OneOf.Types;
 using LeaderboardBackend.Models.Entities;
 
 namespace LeaderboardBackend.Result;
 
 public readonly record struct AccountConfirmed;
 public readonly record struct AlreadyUsed;
+public readonly record struct AlreadyDeleted;
 public readonly record struct BadCredentials;
 public readonly record struct BadRole;
 public readonly record struct ConfirmationNotFound;
@@ -15,3 +18,6 @@ public readonly record struct LeaderboardNeverDeleted;
 public readonly record struct RestoreLeaderboardConflict(Leaderboard Board);
 public readonly record struct UserNotFound;
 public readonly record struct UserBanned;
+
+[GenerateOneOf]
+public partial class DeleteResult : OneOfBase<Success, NotFound, AlreadyDeleted>;
