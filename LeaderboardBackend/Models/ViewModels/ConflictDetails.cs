f63c31a0f91384afda9bd002ef36d1ceb6d193ea
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2025-01-11 17:57:29 -0500

    Create ConflictDetails class.

---
+++ LeaderboardBackend/Models/ViewModels/ConflictDetails.cs
@@ -0,0 +1,12 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaderboardBackend.Models.ViewModels;
+
+/// <summary>
+///     A fake ProblemDetails subclass used for deserialization and documentation. Do not instantiate!
+/// </summary>
+public class ConflictDetails<T> : ProblemDetails {
+    [JsonPropertyName("conflicting")]
+    public T? Conflicting { get; set; }
+}
