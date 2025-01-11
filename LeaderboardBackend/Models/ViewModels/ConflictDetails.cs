ed4ff11d5af7a01763c2faf73297f05b129d5954
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2025-01-11 17:59:02 -0500

    Fix formatting.

--- LeaderboardBackend/Models/ViewModels/ConflictDetails.cs
+++ LeaderboardBackend/Models/ViewModels/ConflictDetails.cs
@@ -6,7 +6,8 @@ namespace LeaderboardBackend.Models.ViewModels;
 /// <summary>
 ///     A fake ProblemDetails subclass used for deserialization and documentation. Do not instantiate!
 /// </summary>
-public class ConflictDetails<T> : ProblemDetails {
+public class ConflictDetails<T> : ProblemDetails
+{
     [JsonPropertyName("conflicting")]
     public T? Conflicting { get; set; }
 }
