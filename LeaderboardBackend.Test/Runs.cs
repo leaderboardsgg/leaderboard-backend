6505bfea0bbfe63c881d49bfa3363ff397d07547
zysim <9867871+zysim@users.noreply.github.com>
2025-02-28 23:21:38 +0800

    Add tests

--- LeaderboardBackend.Test/Runs.cs
+++ LeaderboardBackend.Test/Runs.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Net;
 using System.Threading.Tasks;
 using LeaderboardBackend.Models;
 using LeaderboardBackend.Models.Entities;
 using LeaderboardBackend.Models.Requests;
 using LeaderboardBackend.Models.ViewModels;
+using LeaderboardBackend.Test.Lib;
 using LeaderboardBackend.Test.TestApi;
 using LeaderboardBackend.Test.TestApi.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using NodaTime;
 using NUnit.Framework;
 
@@ -66,20 +69,92 @@ namespace LeaderboardBackend.Test
         }
 
         [Test]
+        public static async Task GetRun_OK()
+        {
+            Run created = await CreateRun(
+                new()
+                {
+                    CategoryId = _categoryId,
+                    Info = "",
+                    PlayedOn = LocalDate.MinIsoValue,
+                    TimeOrScore = 1000,
+                    UserId = TestInitCommonFields.Admin.Id,
+                }
+            );
+
+            await FluentActions.Awaiting(() => _apiClient.Get<TimedRunViewModel>(
+                $"/api/run/{created.Id.ToUrlSafeBase64String()}",
+                new() { }
+            ))
+                .Should()
+                .NotThrowAsync()
+                .WithResult(new()
+                {
+                    Id = created.Id,
+                    Info = created.Info,
+                    PlayedOn = created.PlayedOn,
+                    CreatedAt = created.CreatedAt,
+                    UpdatedAt = created.UpdatedAt,
+                    DeletedAt = created.DeletedAt,
+                    CategoryId = created.CategoryId,
+                    UserId = created.UserId,
+                    Time = created.Time,
+                });
+        }
+
+        [Test]
+        public static async Task GetRun_NotFound_BadRouteParamType() =>
+            await FluentActions.Awaiting(() =>
+                _apiClient.Get<RunViewModel>(
+                "/api/run/1",
+                new() { }
+            )).Should()
+            .ThrowAsync<RequestFailureException>()
+            .Where(e => e.Response.StatusCode == HttpStatusCode.NotFound);
+
+        [Test]
+        public static async Task GetRun_NotFound_RunDoesNotExist() =>
+            await FluentActions.Awaiting(() =>
+                _apiClient.Get<RunViewModel>(
+                $"/api/run/{Guid.Empty.ToUrlSafeBase64String()}",
+                new() { }
+            )).Should()
+            .ThrowAsync<RequestFailureException>()
+            .Where(e => e.Response.StatusCode == HttpStatusCode.NotFound);
+
+        [Test]
         public static async Task CreateRun_OK()
         {
-            RunViewModel created = await CreateRun();
+            Run created = await CreateRun(
+                new()
+                {
+                    CategoryId = _categoryId,
+                    Info = "",
+                    PlayedOn = LocalDate.MinIsoValue,
+                    TimeOrScore = 1000,
+                    UserId = TestInitCommonFields.Admin.Id,
+                }
+            );
 
-            RunViewModel retrieved = await GetRun(created.Id);
+            RunViewModel retrieved = await GetRun<RunViewModel>(created.Id);
 
             created.Should().NotBeNull();
             created.Id.Should().Be(retrieved.Id);
         }
 
         [Test]
-        public static async Task GetCategory_OK()
+        public static async Task GetCategoryForRun_OK()
         {
-            RunViewModel createdRun = await CreateRun();
+            Run createdRun = await CreateRun(
+                new()
+                {
+                    CategoryId = _categoryId,
+                    Info = "",
+                    PlayedOn = LocalDate.MinIsoValue,
+                    TimeOrScore = 1000,
+                    UserId = TestInitCommonFields.Admin.Id,
+                }
+            );
 
             CategoryViewModel category = await _apiClient.Get<CategoryViewModel>(
                 $"api/run/{createdRun.Id.ToUrlSafeBase64String()}/category",
@@ -90,23 +165,17 @@ namespace LeaderboardBackend.Test
             category.Id.Should().Be(_categoryId);
         }
 
-        private static async Task<RunViewModel> CreateRun() =>
-            await _apiClient.Post<RunViewModel>(
-                "/runs/create",
-                new()
-                {
-                    Body = new CreateRunRequest
-                    {
-                        PlayedOn = LocalDate.MinIsoValue,
-                        Info = null,
-                        CategoryId = _categoryId
-                    },
-                    Jwt = _jwt
-                }
-            );
+        private static async Task<Run> CreateRun(Run run)
+        {
+            IServiceScope scope = _factory.Services.CreateScope();
+            ApplicationContext context = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+            await context.AddAsync(run);
+            await context.SaveChangesAsync();
+            return run;
+        }
 
-        private static async Task<RunViewModel> GetRun(Guid id) =>
-            await _apiClient.Get<RunViewModel>(
+        private static async Task<T> GetRun<T>(Guid id) where T : RunViewModel =>
+            await _apiClient.Get<T>(
                 $"/api/run/{id.ToUrlSafeBase64String()}",
                 new() { Jwt = _jwt }
             );
