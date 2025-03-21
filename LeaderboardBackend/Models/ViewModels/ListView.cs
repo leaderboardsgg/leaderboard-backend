8d0d1a146948de78c74422aa85e2f57b63835125
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2025-03-20 21:38:18 -0400

    Rename ItemsTotal to Total.

--- LeaderboardBackend/Models/ViewModels/ListView.cs
+++ LeaderboardBackend/Models/ViewModels/ListView.cs
@@ -3,7 +3,7 @@ namespace LeaderboardBackend.Models.ViewModels;
 public record ListView<T>
 {
     public required IList<T> Data { get; set; }
-    public required long ItemsTotal { get; set; }
+    public required long Total { get; set; }
     public required int LimitDefault { get; set; }
     public required int LimitMax { get; set; }
 }
