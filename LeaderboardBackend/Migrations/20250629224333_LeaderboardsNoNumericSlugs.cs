3213f13bb15badaa9cf30de8545ad4ef4eaf747b
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2025-06-30 17:17:33 -0400

    Apply missing LB slug migration.

---
+++ LeaderboardBackend/Migrations/20250629224333_LeaderboardsNoNumericSlugs.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaderboardBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class LeaderboardsNoNumericSlugs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_leaderboards_slug_RegularExpression",
+                table: "leaderboards");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:run_type", "score,time")
+                .Annotation("Npgsql:Enum:sort_direction", "ascending,descending")
+                .Annotation("Npgsql:Enum:user_role", "administrator,banned,confirmed,registered")
+                .Annotation("Npgsql:PostgresExtension:citext", ",,")
+                .OldAnnotation("Npgsql:Enum:run_type", "time,score")
+                .OldAnnotation("Npgsql:Enum:sort_direction", "ascending,descending")
+                .OldAnnotation("Npgsql:Enum:user_role", "registered,confirmed,administrator,banned")
+                .OldAnnotation("Npgsql:PostgresExtension:citext", ",,");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_leaderboards_slug_RegularExpression",
+                table: "leaderboards",
+                sql: "slug ~ '^(?!([0-9]+)$)[a-zA-Z0-9\\-_]*$'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_leaderboards_slug_RegularExpression",
+                table: "leaderboards");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:run_type", "time,score")
+                .Annotation("Npgsql:Enum:sort_direction", "ascending,descending")
+                .Annotation("Npgsql:Enum:user_role", "registered,confirmed,administrator,banned")
+                .Annotation("Npgsql:PostgresExtension:citext", ",,")
+                .OldAnnotation("Npgsql:Enum:run_type", "score,time")
+                .OldAnnotation("Npgsql:Enum:sort_direction", "ascending,descending")
+                .OldAnnotation("Npgsql:Enum:user_role", "administrator,banned,confirmed,registered")
+                .OldAnnotation("Npgsql:PostgresExtension:citext", ",,");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_leaderboards_slug_RegularExpression",
+                table: "leaderboards",
+                sql: "slug ~ '^[a-zA-Z0-9\\-_]*$'");
+        }
+    }
+}
