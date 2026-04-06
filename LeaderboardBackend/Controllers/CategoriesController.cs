34b1a52308d2be6799357c95bb038646bdec0cbb
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2026-04-06 07:37:54 -0400

    Revert "Use typed results for categories API."
    This reverts commit 08043911e97c65b15ce22fa1fcc7ce7201f6e87c.

--- LeaderboardBackend/Controllers/CategoriesController.cs
+++ LeaderboardBackend/Controllers/CategoriesController.cs
@@ -7,7 +7,6 @@ using LeaderboardBackend.Models.ViewModels;
 using LeaderboardBackend.Result;
 using LeaderboardBackend.Services;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -18,16 +17,18 @@ public class CategoriesController(ICategoryService categoryService) : ApiControl
     [AllowAnonymous]
     [HttpGet("api/categories/{id:long}")]
     [SwaggerOperation("Gets a Category by its ID.", OperationId = "getCategory")]
-    public async Task<Results<Ok<CategoryViewModel>, NotFound>> GetCategory([FromRoute] long id)
+    [SwaggerResponse(200)]
+    [SwaggerResponse(404)]
+    public async Task<ActionResult<CategoryViewModel>> GetCategory([FromRoute] long id)
     {
         Category? category = await categoryService.GetCategory(id);
 
         if (category == null)
         {
-            return TypedResults.NotFound();
+            return NotFound();
         }
 
-        return TypedResults.Ok(CategoryViewModel.MapFrom(category));
+        return Ok(CategoryViewModel.MapFrom(category));
     }
 
     [AllowAnonymous]
@@ -35,7 +36,7 @@ public class CategoriesController(ICategoryService categoryService) : ApiControl
     [SwaggerOperation("Gets a Category of Leaderboard `id` by its slug. Will not return deleted Categories.", OperationId = "getCategoryBySlug")]
     [SwaggerResponse(200)]
     [SwaggerResponse(404, "The Category either doesn't exist for the Leaderboard, or it has been deleted.", typeof(ProblemDetails))]
-    public async Task<Results<Ok<CategoryViewModel>, NotFound>> GetCategoryBySlug(
+    public async Task<ActionResult<CategoryViewModel>> GetCategoryBySlug(
         [FromRoute] long id,
         [FromRoute] string slug
     )
@@ -44,17 +45,20 @@ public class CategoriesController(ICategoryService categoryService) : ApiControl
 
         if (category == null)
         {
-            return TypedResults.NotFound();
+            return NotFound();
         }
 
-        return TypedResults.Ok(CategoryViewModel.MapFrom(category));
+        return Ok(CategoryViewModel.MapFrom(category));
     }
 
     [AllowAnonymous]
     [HttpGet("api/leaderboards/{id:long}/categories")]
     [Paginated]
     [SwaggerOperation("Gets all Categories of Leaderboard `id`.", OperationId = "getCategoriesForLeaderboard")]
-    public async Task<Results<Ok<ListView<CategoryViewModel>>, NotFound, UnprocessableEntity<ValidationProblemDetails>>> GetCategoriesForLeaderboard(
+    [SwaggerResponse(200)]
+    [SwaggerResponse(404, "The Leaderboard with ID `id` could not be found.", typeof(ProblemDetails))]
+    [SwaggerResponse(422, Type = typeof(ValidationProblemDetails))]
+    public async Task<ActionResult<ListView<CategoryViewModel>>> GetCategoriesForLeaderboard(
         [FromRoute] long id,
         [FromQuery] Page page,
         [FromQuery] StatusFilter status = StatusFilter.Published
@@ -62,62 +66,50 @@ public class CategoriesController(ICategoryService categoryService) : ApiControl
     {
         GetCategoriesForLeaderboardResult r = await categoryService.GetCategoriesForLeaderboard(id, status, page);
 
-        return r.Match<Results<Ok<ListView<CategoryViewModel>>, NotFound, UnprocessableEntity<ValidationProblemDetails>>>(
-            categories => TypedResults.Ok(new ListView<CategoryViewModel>
+        return r.Match<ActionResult<ListView<CategoryViewModel>>>(
+            categories => Ok(new ListView<CategoryViewModel>
             {
                 Data = [.. categories.Items.Select(CategoryViewModel.MapFrom)],
                 Total = categories.ItemsTotal
             }),
-            notFound => TypedResults.NotFound()
+            notFound => NotFound()
         );
     }
 
-    // This endpoint must omit one of its possible result types because it only goes up to 6 generic type args.
-    // Hopefully this gets fixed soon. PR to address this issue:
-    // https://github.com/dotnet/aspnetcore/pull/66092
-    // —Ted W
     [Authorize(Policy = UserTypes.ADMINISTRATOR)]
     [HttpPost("leaderboards/{id:long}/categories")]
     [SwaggerOperation("Creates a new Category for a Leaderboard with ID `id`. This request is restricted to Administrators.", OperationId = "createCategory")]
-    [SwaggerResponse(400, Type = typeof(ProblemDetails))]
+    [SwaggerResponse(201)]
     [SwaggerResponse(401)]
     [SwaggerResponse(403)]
     [SwaggerResponse(404, "The Leaderboard with ID `id` could not be found.")]
     [SwaggerResponse(409, "A Category with the specified slug already exists.", typeof(ConflictDetails<CategoryViewModel>))]
     [SwaggerResponse(422, $"The request contains errors. The following errors can occur: NotEmptyValidator, {SlugRule.SLUG_FORMAT}", typeof(ValidationProblemDetails))]
-    public async Task<Results<
-        CreatedAtRoute<CategoryViewModel>,
-        UnauthorizedHttpResult,
-        ForbidHttpResult,
-        NotFound,
-        Microsoft.AspNetCore.Http.HttpResults.Conflict<ProblemDetails>,
-        UnprocessableEntity<ValidationProblemDetails>
-    >> CreateCategory(
+    public async Task<ActionResult<CategoryViewModel>> CreateCategory(
         [FromRoute] long id,
         [FromBody, SwaggerRequestBody(Required = true)] CreateCategoryRequest request
     )
     {
         CreateCategoryResult r = await categoryService.CreateCategory(id, request);
 
-        return r.Match<Results<
-            CreatedAtRoute<CategoryViewModel>,
-            UnauthorizedHttpResult,
-            ForbidHttpResult,
-            NotFound,
-            Microsoft.AspNetCore.Http.HttpResults.Conflict<ProblemDetails>,
-            UnprocessableEntity<ValidationProblemDetails>
-        >>(
-            category => TypedResults.CreatedAtRoute(
-                CategoryViewModel.MapFrom(category),
+        return r.Match<ActionResult<CategoryViewModel>>(
+            category => CreatedAtAction(
                 nameof(GetCategory),
-                new { id = category.Id }),
+                new { id = category.Id },
+                CategoryViewModel.MapFrom(category)
+            ),
             conflict =>
                 {
                     ProblemDetails problemDetails = ProblemDetailsFactory.CreateProblemDetails(HttpContext, StatusCodes.Status409Conflict);
                     problemDetails.Extensions.Add("conflicting", CategoryViewModel.MapFrom(conflict.Conflicting));
-                    return TypedResults.Conflict(problemDetails);
+                    return Conflict(problemDetails);
                 },
-            notFound => TypedResults.NotFound()
+            notFound => Problem(
+                null,
+                null,
+                404,
+                "Leaderboard Not Found"
+            )
         );
     }
 
@@ -130,51 +122,39 @@ public class CategoriesController(ICategoryService categoryService) : ApiControl
         "All fields of the request body are optional but you must specify at least one.",
         OperationId = "updateCategory"
     )]
-    [SwaggerResponse(400, Type = typeof(ProblemDetails))]
+    [SwaggerResponse(204)]
     [SwaggerResponse(401)]
     [SwaggerResponse(403)]
+    [SwaggerResponse(404, Type = typeof(ProblemDetails))]
     [SwaggerResponse(
         409,
         "The specified slug is already in use by another category. Returns the conflicting category.",
         typeof(ConflictDetails<CategoryViewModel>)
     )]
     [SwaggerResponse(422, Type = typeof(ValidationProblemDetails))]
-    public async Task<Results<
-        NoContent,
-        UnauthorizedHttpResult,
-        ForbidHttpResult,
-        NotFound,
-        Microsoft.AspNetCore.Http.HttpResults.Conflict<ProblemDetails>,
-        UnprocessableEntity<ValidationProblemDetails>
-    >> UpdateCategory(
+    public async Task<ActionResult> UpdateCategory(
         [FromRoute] long id,
         [FromBody, SwaggerRequestBody(Required = true)] UpdateCategoryRequest request
     )
     {
         UpdateResult<Category> res = await categoryService.UpdateCategory(id, request);
 
-        return res.Match<Results<
-            NoContent,
-            UnauthorizedHttpResult,
-            ForbidHttpResult,
-            NotFound,
-            Microsoft.AspNetCore.Http.HttpResults.Conflict<ProblemDetails>,
-            UnprocessableEntity<ValidationProblemDetails>
-        >>(
+        return res.Match<ActionResult>(
             conflict =>
             {
                 ProblemDetails problemDetails = ProblemDetailsFactory.CreateProblemDetails(HttpContext, StatusCodes.Status409Conflict);
                 problemDetails.Extensions.Add("conflicting", CategoryViewModel.MapFrom(conflict.Conflicting));
-                return TypedResults.Conflict(problemDetails);
+                return Conflict(problemDetails);
             },
-            notFound => TypedResults.NotFound(),
-            success => TypedResults.NoContent()
+            notFound => NotFound(),
+            success => NoContent()
         );
     }
 
     [Authorize(Policy = UserTypes.ADMINISTRATOR)]
     [HttpDelete("categories/{id:long}")]
     [SwaggerOperation("Deletes a Category. This request is restricted to Administrators.", OperationId = "deleteCategory")]
+    [SwaggerResponse(204)]
     [SwaggerResponse(401)]
     [SwaggerResponse(403)]
     [SwaggerResponse(
@@ -185,26 +165,14 @@ public class CategoriesController(ICategoryService categoryService) : ApiControl
         """,
         typeof(ProblemDetails)
     )]
-    public async Task<Results<
-        NoContent,
-        UnauthorizedHttpResult,
-        ForbidHttpResult,
-        ProblemHttpResult,
-        NotFound
-    >> DeleteCategory([FromRoute] long id)
+    public async Task<ActionResult> DeleteCategory([FromRoute] long id)
     {
         DeleteResult res = await categoryService.DeleteCategory(id);
 
-        return res.Match<Results<
-            NoContent,
-            UnauthorizedHttpResult,
-            ForbidHttpResult,
-            ProblemHttpResult,
-            NotFound
-        >>(
-            success => TypedResults.NoContent(),
-            notFound => TypedResults.NotFound(),
-            alreadyDeleted => TypedResults.Problem(
+        return res.Match<ActionResult>(
+            success => NoContent(),
+            notFound => NotFound(),
+            alreadyDeleted => Problem(
                 null,
                 null,
                 404,
