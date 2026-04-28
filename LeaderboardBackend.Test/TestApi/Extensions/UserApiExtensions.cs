e5db969a2d6da6b47846e4a646201259bdd844a0
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2026-04-27 20:31:01 -0400

    Remove unnecessary using.

--- LeaderboardBackend.Test/TestApi/Extensions/UserApiExtensions.cs
+++ LeaderboardBackend.Test/TestApi/Extensions/UserApiExtensions.cs
@@ -2,7 +2,6 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using LeaderboardBackend.Models.Requests;
-using LeaderboardBackend.Models.ViewModels;
 using LeaderboardBackend.Test.Lib;
 
 namespace LeaderboardBackend.Test.TestApi.Extensions;
